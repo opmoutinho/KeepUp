e300ec3ab1da14db4fb7994d779d0a8a0478bf7c
Andre Santos <candre.csantos@gmail.com>
2021-02-12 22:10:32 +0000

    Corrected the sprite management. Commit for merge

--- src/org/academiadecodigo/timemaravilha/Main.java
+++ src/org/academiadecodigo/timemaravilha/Main.java
@@ -1,14 +1,5 @@
 package org.academiadecodigo.timemaravilha;
 
-import org.academiadecodigo.simplegraphics.graphics.Canvas;
-import org.academiadecodigo.simplegraphics.graphics.Shape;
-import org.academiadecodigo.simplegraphics.pictures.Picture;
-import org.academiadecodigo.timemaravilha.entities.EntityManager;
-import org.academiadecodigo.timemaravilha.entities.Player;
-import org.academiadecodigo.timemaravilha.grid.SimpleGfxGrid;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException{
